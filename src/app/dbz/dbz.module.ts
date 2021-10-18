478dd519336a1581ebc0da753368a1ef36eac945
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-10-18 15:51:45 -0500

    incorporación del formulario

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    PersonajesComponent
   ],
   exports: [
     MainPageComponent
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -6,9 +7,31 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent  {
 
-  agregar() {
-    console.log('Hey');
-    
-  }
+  personajes: Array<Personaje> = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'vegeta',
+      poder: 2000
+    }
+  ];
 
+  nuevo:Personaje = {
+    nombre: '', 
+    poder: 0
+  };
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) { return; }
+
+    console.log( this.nuevo );
+
+    this.personajes.push(this.nuevo)
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
 };
